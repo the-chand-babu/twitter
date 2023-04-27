145690b1fac6cef62b9722afb739c45cd138d11b
ApoorvVerma98 <apoorv.verma9@gmail.com>
2023-04-27 20:00:11 +0530

    SearchBar-Done

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,22 +1,21 @@
-import style from './home.module.css'
-import React from 'react'
+import style from "./home.module.css";
+import React from "react";
+import Search from "../../component/rightSideBar/searchbar/SearchBar";
+import Trends from "../../component/rightSideBar/whatsHappening/WhatsNew";
+
 
 const Home = () => {
   return (
     <div className={style.homeContainer}>
+      <div className={style.sidebar}>sidebar... invoks</div>
 
-       <div className={style.sidebar}>
-        sidebar... invoks
-       </div>
-
-       <div className={style.mainContainer}>
-      main tweets component involks
-       </div>
-       <div>
-            whats new and search bar here...
-       </div>
+      <div className={style.mainContainer}>main tweets component involks</div>
+      <div>
+        <Search />
+        <Trends />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
