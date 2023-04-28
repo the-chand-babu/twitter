9cf3c610139621726b4b0e6585452a06b3b9227c
chand babu <jafri.nbj@gmail.com>
2023-04-28 23:37:38 +0530

    tweet

---
+++ src/component/leftsidebar/LeftSideBar.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Leftsidebar = () => {
+  return (
+    <div>
+      this is side bar
+    </div>
+  )
+}
+
+export default Leftsidebar
--- src/component/liveTweet/LiveTweet.jsx
+++ src/component/liveTweet/LiveTweet.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Tweet from "../liveTweetFeed/Tweet";
 import { useDispatch, useSelector } from "react-redux";
 import { TweetSlice } from "../storeComponent/reducer";
 import style from "./LiveTweet.module.css";
+import FlipMove from "react-flip-move";
+import PostTweet from "../postTweet/PostTweet";
+
 
 function LiveTweet() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("http://localhost:3000/Tweet.json")
+    fetch("/Tweet.json")
       .then((res) => res.json())
       .then((res) => dispatch(TweetSlice.actions.AddAllTweet(res)));
   }, []);
@@ -16,10 +19,23 @@ function LiveTweet() {
   const TweetData = useSelector((state) => state.Tweets);
 
   return (
-    <div className={style.TweetContainer}>
-      {TweetData.map((tweet, index) => {
-        return <Tweet key={index} tweet={tweet} index={index} />;
-      })}
+    <div className={style.feed}>
+      <div className={style.feed__header}>
+        <h2>Home</h2>
+      </div>
+
+      <PostTweet />
+
+      <FlipMove>
+        {
+          TweetData.map((tweet,index)=>{
+            return (
+              <Tweet key={index} tweet={tweet} index={index} />
+
+            )
+          })
+        }
+      </FlipMove>
     </div>
   );
 }
